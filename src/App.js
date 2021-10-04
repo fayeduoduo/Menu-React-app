441fedefa7c0bb6b1e54b4bf47f43af5b46417b2
fayeduoduo <fayeduoduo@gmail.com>
2021-10-03 19:11:42 -0700

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { useState } from 'react';
 import Categories from './Components/Categories';
 import Menu from './Components/Menu';
-// items是随意取的名字，也可以叫menus
 import items from './data';
 
 
-//const allCategories = items.map(item => item.category)
-//console.log(allCategories)
-//数组相同元素的去重, es6方法
-//也可以通过es6方法，在数组中添加新数据
 const allCategories = ['all',...new Set(items.map(item => item.category))]
 console.log(allCategories)
 function App() {
   const [menuItems, setMenuItems] = useState(items)
-  const [categories,setCategories] = useState(allCategories)
-  
-  //实现过滤
+  const [categories,setCategories] = useState(allCategories) 
+
   const filterMenu = (btnCategory) => {
-    //设置全部item显示
     if (btnCategory === 'all') {
       setMenuItems(items);
       return
